af4fde78cc6cbd10d37339309349fbc9857f9d7b
im <morigo.2015@gmail.com>
2018-12-04 14:41:46 +0200

    ping interv_timer

--- ping.py
+++ ping.py
@@ -1,11 +1,54 @@
 # ping
 
 import subprocess
-import os
 import datetime
 import time
 
 from my_db import MyDb
+from interv_timer import IntervTimer
+
+class DbPing(MyDb):
+    """ actions with ping table in MySql """
+
+    def __init__(self):
+        super().__init__()
+
+    def create_table(self, show=True):
+        query = """
+            create table if not exists ping (
+              host varchar(20),
+              time datetime,
+              ok bool,
+              loss float,
+              min_rtt float,
+              avg_rtt float,
+              max_rtt float,
+              mdev_rtt float
+              )
+              """
+        self.exec(query, show=show)
+
+    def count_ping_items(self) -> int:
+        query = """
+            select count(*)
+            from ping
+        """
+        res = self.exec(query, show=False)
+        return int(res[0][0])  # there is 1-line/1-column in res
+
+    def insert_ping_item(self, ping_res, show=False):
+        query = f"""
+            insert into ping values(
+                "{ping_res['host']}", "{ping_res['time']}", {self.bool_2_sql(ping_res['OK'])},
+                "{ping_res['loss(%)']}", 
+                "{ping_res['min']}","{ping_res['avg']}","{ping_res['max']}","{ping_res['mdev']}"
+            )
+        """
+        # print(query)
+        self.exec(query, show=show)
+
+
+import time
 
 hosts = {
     "external": "www.ua",
@@ -14,13 +57,12 @@ hosts = {
     "bullet": "192.168.1.70",
     "door_bell": "192.168.1.165"
 }
+sleep_interv_sec = 60.0
 
 
 def ping(host: str, n: int = 3):
-    results = {"host": host, "time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-               "OK": False,
-               "loss(%)": 100,
-               "min": -1.0, "avg": -1.0, "max": -1.0, "mdev": -1.0,
+    results = {"host": host, "time": MyDb.now_timestamp(),
+               "OK": False, "loss(%)": 100, "min": -1.0, "avg": -1.0, "max": -1.0, "mdev": -1.0,  # disconnected
                }
     try:
         ping_answer: str = subprocess.check_output(
@@ -30,62 +72,33 @@ def ping(host: str, n: int = 3):
             stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError:
         # results["OK"] = False
-        # by default the results have already been set to connection broken
+        # by default the results have already been set to disconnected
         pass
     else:
         results["OK"] = True
         answer_lines = ping_answer.splitlines(keepends=False)
+        # penult line - take losses percentage
         results["loss(%)"] = int(answer_lines[-2].split(sep=' ')[5][:-1])
+        # last line - take rtt indicators
         rtt_indicators = answer_lines[-1].split(sep=' ')[1].split(sep='/')
         rtt_values = answer_lines[-1].split(sep=' ')[3].split(sep='/')
         for indicator, value in zip(rtt_indicators, rtt_values):
             results[indicator] = float(value)
     return results
 
-def create_table(db:MyDb):
-    query = """
-        create table if not exists ping (
-          host varchar(20),
-          time datetime,
-          ok bool,
-          loss float,
-          min_rtt float,
-          avg_rtt float,
-          max_rtt float,
-          mdev_rtt float
-          )
-          """
-    db.exec(query)
-
-def count_ping_items(db:MyDb):
-    query = """
-        select count(*)
-        from ping
-    """
-    db.exec(query)
-
-def insert_ping_item(db:MyDb,ping_res):
-    query = f"""
-        insert into ping values(
-            "{ping_res['host']}", "{ping_res['time']}", {str(ping_res['OK']).upper()},
-            "{ping_res['loss(%)']}", 
-            "{ping_res['min']}","{ping_res['avg']}","{ping_res['max']}","{ping_res['mdev']}"
-        )
-    """
-    # print(query)
-    db.exec(query)
 
 def main():
-    db = MyDb()
-    create_table(db)
-    count_ping_items(db)
+    db = DbPing()
+    db.create_table(show=False)
+    print(f" There are {db.count_ping_items()} items in ping table")
+    it = IntervTimer(sleep_interv_sec) # interval timer to keep interval between awakes regardless of delays
     while True:
         for host_name in hosts:
             res = ping(hosts[host_name])
-
-            insert_ping_item(db,res)
+            db.insert_ping_item(res, show=False)
             print(datetime.datetime.now(), res)
-        time.sleep(10)
+        # time.sleep(sleep_interv_sec)
+        it.wait_interval()
 
 
 if __name__ == "__main__":
